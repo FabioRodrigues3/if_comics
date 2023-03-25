ab060af5809425b3d95e2d86c3dee80019f99a7f
fabio.rodrigues <fabio.rodrigues@midgard.local>
2023-03-25 00:49:29 -0300

    Add base project

---
+++ home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect, get_object_or_404
+
+def home(request):
+    return render(request, 'home.html')
+
+def webcomic(request):
+    return render(request, 'webcomic.html')
+
